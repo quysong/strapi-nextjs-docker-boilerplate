7cb9e843d1ac914fe0015dbfb0c1dc814db17d93
Anh Ly <anh.ly@credify.one>
2023-01-30 22:29:23 +0700

    add auth0

---
+++ frontend/components/Content.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Content = () => (
+  <div className="next-steps my-5" data-testid="content">
+    <h2 className="my-5 text-center" data-testid="content-title">
+      What can I do next?
+    </h2>
+  </div>
+);
+
+export default Content;
