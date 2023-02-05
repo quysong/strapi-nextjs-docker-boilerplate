205ebc9ed67027e74977013973e6f9c33c529d4a
Anh Ly <anh.ly@credify.one>
2023-02-05 21:05:28 +0700

    add logic custom after callback

--- frontend/pages/api/auth/[...auth0].ts
+++ frontend/pages/api/auth/[...auth0].ts
@@ -1,3 +1,34 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin, handleCallback, getSession } from "@auth0/nextjs-auth0";
 
-export default handleAuth();
+const afterCallback = (req:any , res:any , session:any , state:any ) => {
+  console.log('afterCallback req', req)
+  console.log('afterCallback res', res)
+  console.log('afterCallback session', session)
+  console.log('afterCallback state', state)
+  return session;
+};
+
+export default handleAuth({
+  async login(req, res) {
+    try {
+      // Pass in custom params to your handler
+      await handleLogin(req, res, {
+        authorizationParams: { customParam: "foo" },
+      });
+      // Add your own custom logging.
+    } catch (error: any) {
+      // Add you own custom error logging.
+      res.status(error.status || 500).end();
+    }
+  },
+  async callback(req, res) {
+    try {
+      // Pass in custom params to your handler
+      await handleCallback(req, res, { afterCallback });
+      // Add your own custom logging.
+    } catch (error: any) {
+      // Add you own custom error logging.
+      res.status(error.status || 500).end();
+    }
+  },
+});
